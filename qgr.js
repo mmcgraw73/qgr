8d493dd27e39521bc1160dd7dbb0458b1ce4b90c
Michael McGraw <mm380v@att.com>
2018-01-03 11:22:22 -0500

    initial commit

--- qgr.js
+++ qgr.js
@@ -2,3 +2,22 @@
 
 const replace = require('replace');
 const exec = require('child_process').exec;
+const pathArg = process.argv.slice(2);
+const findArg = process.argv.slice(3);
+const replaceArg = process.argv.slice(4);
+
+//const searchPattern = "-r -l 'localhost:1111/sit' ./content/src/main/content/jcr_root/etc/designs/zig";
+
+const qgrInit = exec('grep -l -r' + findArg + pathArg, function(err,stdout,stderr) {
+  let arr = stdout.split("\n");
+  arr.pop();
+  arr.length <= 0 ? console.log('fail, not found: ', findArg) : console.log('success, files found: ', arr.length);
+  console.log('files updated:\n', arr);
+  replace({
+      regex: findArg,
+      replacement: replaceArg,
+      paths: arr,
+      recursive: true,
+      silent: true
+  });
+});
